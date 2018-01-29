1be9175ec2750a7de545c0cec5471b8a740e5427
sulistiyanto <sulistiyanto404@gmail.com>
2018-01-29 21:58:32 +0700

    lesson 04

---
+++ app/src/main/java/com/tubandev/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.tubandev.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarImageClicked(view: View) {
+
+    }
+
+    fun generateBackgroundColorClicked(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+}
