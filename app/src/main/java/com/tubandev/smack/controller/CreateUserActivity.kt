2108a8d5d3c642a1d17824eb2a9a0ba530bb8563
Sulistiyanto <sulistiyanto404@gmail.com>
2018-02-05 03:45:52 +0700

    lesson 13

--- app/src/main/java/com/tubandev/smack/controller/CreateUserActivity.kt
+++ app/src/main/java/com/tubandev/smack/controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package com.tubandev.smack.controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import com.tubandev.smack.R
 import com.tubandev.smack.services.AuthService
 import com.tubandev.smack.services.UserDataService
+import com.tubandev.smack.utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,6 +22,7 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        progressBar.visibility = View.INVISIBLE
     }
 
     fun createAvatarImageClicked(view: View) {
@@ -46,24 +51,56 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        enableProgressBar(true)
         val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
 
-        AuthService.registerUser(this, email, password) { registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) {loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
-                            if (createSuccess) {
-                                println(UserDataService.name)
-                                finish()
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, email, password) { registerSuccess ->
+                if (registerSuccess) {
+                    enableProgressBar(false)
+                    AuthService.loginUser(this, email, password) {loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
+                                if (createSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    println(UserDataService.name)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
-            }
 
+            }
+        } else {
+            Toast.makeText(this, "Make sure username, email and password are filled in", Toast.LENGTH_SHORT).show()
+            enableProgressBar(false)
         }
+    }
+
+    fun enableProgressBar(enable : Boolean) {
+        if (enable) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.INVISIBLE
+        }
+
+        createUserBtn.isEnabled = !enable
+        createAvatarImage.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again", Toast.LENGTH_SHORT).show()
+        enableProgressBar(false)
     }
 }
--- app/src/main/java/com/tubandev/smack/utilities/Constants.kt
+++ app/src/main/java/com/tubandev/smack/utilities/Constants.kt
@@ -8,4 +8,7 @@ package com.tubandev.smack.utilities
 const val BASE_URL = "https://tubandevchat2.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
-const val URL_CREATE_USER = "${BASE_URL}account/add"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+// Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
